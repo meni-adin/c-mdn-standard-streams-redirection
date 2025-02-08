4a878759dd23dcc567e3dd10de5abe0868abc550
Menachem Adin <meniadin@gmail.com>
2025-02-08 15:08:20 +0200

    Run clang-format

--- src/standard_streams_redirection/include/standard_streams_redirection.h
+++ src/standard_streams_redirection/include/standard_streams_redirection.h
@@ -3,32 +3,33 @@
 #define STANDARD_STREAMS_REDIRECTION_H
 
 #if defined(__linux__) || defined(__APPLE__)
- #include <fcntl.h>
- #include <unistd.h>
- #define CROSS_OS_dup(...) dup(__VA_ARGS__)
- #define CROSS_OS_dup2(...) dup2(__VA_ARGS__)
- #define CROSS_OS_fileno(...) fileno(__VA_ARGS__)
+# include <fcntl.h>
+# include <unistd.h>
+# define CROSS_OS_dup(...)    dup(__VA_ARGS__)
+# define CROSS_OS_dup2(...)   dup2(__VA_ARGS__)
+# define CROSS_OS_fileno(...) fileno(__VA_ARGS__)
 #elif defined(_WIN32)
- #include <io.h>
- #define CROSS_OS_dup(...) _dup(__VA_ARGS__)
- #define CROSS_OS_dup2(...) _dup2(__VA_ARGS__)
- #define CROSS_OS_fileno(...) _fileno(__VA_ARGS__)
-#endif // OS
+# include <io.h>
+# define CROSS_OS_dup(...)    _dup(__VA_ARGS__)
+# define CROSS_OS_dup2(...)   _dup2(__VA_ARGS__)
+# define CROSS_OS_fileno(...) _fileno(__VA_ARGS__)
+#endif  // OS
 #include <stdbool.h>
+
 #include "errors.h"
 
 #ifdef __cplusplus
 extern "C" {
-#endif // __cplusplus
+#endif  // __cplusplus
 
 // void StandardStreamsRedirection_init(const char *tempFilePrefix, bool separateStderr, bool keepTempFiles);
 status_t StandardStreamsRedirection_init(void);
 status_t StandardStreamsRedirection_deinit(void);
-void StandardStreamsRedirection_startRedirection(void);
-void StandardStreamsRedirection_stopRedirection(void);
+void     StandardStreamsRedirection_startRedirection(void);
+void     StandardStreamsRedirection_stopRedirection(void);
 
 #ifdef __cplusplus
 }
-#endif // __cplusplus
+#endif  // __cplusplus
 
-#endif // STANDARD_STREAMS_REDIRECTION_H
+#endif  // STANDARD_STREAMS_REDIRECTION_H
